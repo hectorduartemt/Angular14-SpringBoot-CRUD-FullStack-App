b9d2fc9efb7cf5f01c7b6348529e5db5d01f5ece
Leonardo <91922519+hectorduartemt@users.noreply.github.com>
2022-07-30 22:06:14 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'Angular 14 Spring Boot CRUD FullStack App';
 }
